97455f4061a592d990262b09b3e633c2699987c2
liobio <582437980@qq.com>
2023-03-13 15:25:18 +0800

    first commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace FormulationQuantification
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.quantization = new System.Windows.Forms.Button();
+            this.numTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.itemComboBox = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.formulaTreeView = new System.Windows.Forms.TreeView();
+            this.formulaListBox = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.quantization);
+            this.panel1.Controls.Add(this.numTextBox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.itemComboBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(584, 100);
+            this.panel1.TabIndex = 6;
+            // 
+            // quantization
+            // 
+            this.quantization.Location = new System.Drawing.Point(384, 38);
+            this.quantization.Name = "quantization";
+            this.quantization.Size = new System.Drawing.Size(53, 23);
+            this.quantization.TabIndex = 10;
+            this.quantization.Text = "量化";
+            this.quantization.UseVisualStyleBackColor = true;
+            this.quantization.Click += new System.EventHandler(this.Quantification);
+            // 
+            // numTextBox
+            // 
+            this.numTextBox.Location = new System.Drawing.Point(263, 38);
+            this.numTextBox.Name = "numTextBox";
+            this.numTextBox.Size = new System.Drawing.Size(80, 23);
+            this.numTextBox.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(207, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "数量";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(19, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "物品";
+            // 
+            // itemComboBox
+            // 
+            this.itemComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.itemComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.itemComboBox.FormattingEnabled = true;
+            this.itemComboBox.Location = new System.Drawing.Point(75, 36);
+            this.itemComboBox.Name = "itemComboBox";
+            this.itemComboBox.Size = new System.Drawing.Size(120, 25);
+            this.itemComboBox.TabIndex = 6;
+            this.itemComboBox.TextUpdate += new System.EventHandler(this.SelectItem);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel2.Controls.Add(this.formulaTreeView);
+            this.panel2.Controls.Add(this.formulaListBox);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(584, 661);
+            this.panel2.TabIndex = 7;
+            // 
+            // formulaTreeView
+            // 
+            this.formulaTreeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formulaTreeView.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.formulaTreeView.Location = new System.Drawing.Point(0, 0);
+            this.formulaTreeView.Name = "formulaTreeView";
+            this.formulaTreeView.Size = new System.Drawing.Size(584, 470);
+            this.formulaTreeView.TabIndex = 1;
+            // 
+            // formulaListBox
+            // 
+            this.formulaListBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.formulaListBox.FormattingEnabled = true;
+            this.formulaListBox.ItemHeight = 17;
+            this.formulaListBox.Location = new System.Drawing.Point(0, 470);
+            this.formulaListBox.Name = "formulaListBox";
+            this.formulaListBox.Size = new System.Drawing.Size(584, 191);
+            this.formulaListBox.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 761);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Rise of Industry 配方量化";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button quantization;
+        private TextBox numTextBox;
+        private Label label2;
+        private Label label1;
+        private ComboBox itemComboBox;
+        private Panel panel2;
+        private ListBox formulaListBox;
+        private TreeView formulaTreeView;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,299 @@
+﻿using System.Data;
+using System.Reflection;
+using LitJson;
+
+
+namespace FormulationQuantification
+{
+    public partial class MainForm : Form
+    {
+        List<Item> items = new List<Item>();
+        string[] itemsName;
+        public MainForm()
+        {
+            InitializeComponent();
+            parentNode = formulaTreeView.Nodes;
+
+        }
+        /// <summary>
+        /// 通过json配置 让item全部实例化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string json = String.Empty;
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string resourceName = assembly.GetName().Name.ToString() + ".Formula.json";
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader sr = new StreamReader(stream))
+                {
+                    json = sr.ReadToEnd();
+                }
+            }
+            JsonData jsonData = JsonMapper.ToObject(json);
+            foreach (JsonData temp in jsonData)
+            {
+                List<Formula> FormulaList = new();
+                //通过字符串索引器可以取得键值对的值
+                JsonData nameValue = temp["name"];
+                JsonData factoryValue = temp["factory"];
+                JsonData formulasValue = temp["formulas"];
+                if (formulasValue != null)
+                {
+                    foreach (JsonData formula in formulasValue)
+                    {
+                        JsonData formulaItemValue = formula["item"];
+                        JsonData formulaNumValue = formula["num"];
+                        string formulaItem = formulaItemValue.ToString();
+                        int formulaNum = int.Parse(formulaNumValue.ToString());
+                        Item tmpItem = items.Find(c => c.Name.Equals(formulaItem));
+                        if (tmpItem != null)
+                        {
+                            FormulaList.Add(new Formula(tmpItem, formulaNum));
+                        }
+                    }
+                }
+                JsonData numValue = temp["num"];
+                string name = nameValue.ToString();
+                string factory = factoryValue.ToString();
+                int num = int.Parse(numValue.ToString());
+                items.Add(new Item(name, factory, FormulaList, num));
+
+            }
+            itemsName = new string[items.Count];
+            for (int i = 0; i < items.Count; i++)
+            {
+                itemsName[i] = items[i].Name;
+            }
+            itemComboBox.Items.AddRange(itemsName);
+        }
+
+        List<Factory> finalFormulas = new();
+        TreeNodeCollection parentNode;
+        Item tmpItem;
+        /// <summary>
+        /// 量化按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Quantification(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(itemComboBox.Text) || String.IsNullOrEmpty(numTextBox.Text))
+            {
+                formulaListBox.Items.Add(System.DateTime.Now + ": 没有选择量化物品或者填写数量！");
+                return;
+            }
+            formulaTreeView.Nodes.Clear();
+            parentNode = formulaTreeView.Nodes;
+            string itemName = itemComboBox.Text;
+            int num = int.Parse(numTextBox.Text);
+
+            tmpItem = items.Find(c => c.Name.Equals(itemName));
+            QuantificationFactory(tmpItem, num);
+            formulaTreeView.ExpandAll();
+        }
+        /// <summary>
+        /// 量化item与factory的生产关系
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="num"></param>
+        public void QuantificationFactory(Item item, float num)
+        {
+            float factoryNum = num / item.Num;
+            finalFormulas.Add(new Factory(item.Name, num, item.Factory, factoryNum));
+            TreeNode treeNode = new TreeNode
+            {
+                Text = "[" + item.Name + ":" + num + "],  [" + item.Factory + ":" + factoryNum + "]"
+            };
+            parentNode.Add(treeNode);
+
+            if (item.Formulas.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                parentNode = treeNode.Nodes;
+                foreach (Formula i in item.Formulas)
+                {
+                    QuantificationFactory(i.Item, i.Num * factoryNum);
+                    parentNode = treeNode.Nodes;
+                }
+
+            }
+
+        }
+        private void SelectItem(object sender, EventArgs e)
+        {
+            var input = itemComboBox.Text.ToUpper();
+            itemComboBox.Items.Clear();
+            if (string.IsNullOrWhiteSpace(input))
+                itemComboBox.Items.AddRange(itemsName);
+            else
+            {
+                var newList = itemsName.Where(x => x.IndexOf(input, StringComparison.CurrentCultureIgnoreCase) != -1).ToArray();
+                itemComboBox.Items.AddRange(newList);
+            }
+            itemComboBox.Select(itemComboBox.Text.Length, 0);
+            itemComboBox.DroppedDown = true;
+            //保持鼠标指针形状  
+            Cursor = Cursors.Default;
+        }
+    }
+    public class Item
+    {
+        private string _name;
+        private string _factory;
+        private float _num;
+        private List<Formula>? formulas = new();
+        public string Name { get => _name; set => _name = value; }
+        public string Factory { get => _factory; set => _factory = value; }
+        public float Num { get => _num; set => _num = value; }
+        internal List<Formula>? Formulas { get => formulas; set => formulas = value; }
+
+        public Item(string name, string factory, List<Formula> dic, int num)
+        {
+            this.Name = name;
+            this.Factory = factory;
+            this.Formulas = dic;
+            this.Num = num;
+        }
+        public Item() { }
+
+    }
+
+    public class Formula
+    {
+
+        private Item _item;
+        private float _num;
+
+        public Item Item { get => _item; set => _item = value; }
+        public float Num { get => _num; set => _num = value; }
+        public Formula() { }
+        public Formula(Item item, float num)
+        {
+            Item = item;
+            Num = num;
+        }
+    }
+    public class Factory
+    {
+
+        private string _itemName;
+        private float _itemNum;
+        private string _factoryName;
+        private float _factoryNum;
+
+
+
+        public Factory() { }
+
+        public Factory(string itemName, float itemNum, string factoryName, float factoryNum)
+        {
+            ItemName = itemName;
+            ItemNum = itemNum;
+            FactoryName = factoryName;
+            FactoryNum = factoryNum;
+        }
+        public override string ToString()
+        {
+            return "[" + _itemName + ":" + _itemNum + "," + _factoryName + ":" + _factoryNum + "]";
+        }
+
+        public string ItemName { get => _itemName; set => _itemName = value; }
+        public float ItemNum { get => _itemNum; set => _itemNum = value; }
+        public string FactoryName { get => _factoryName; set => _factoryName = value; }
+        public float FactoryNum { get => _factoryNum; set => _factoryNum = value; }
+    }
+    //public class FormulaTree<T>
+    //{
+    //    public FormulaTree()
+    //    {
+    //        nodes = new List<FormulaTree<T>>();
+    //    }
+
+    //    public FormulaTree(T data)
+    //    {
+    //        this.Data = data;
+    //        nodes = new List<FormulaTree<T>>();
+    //    }
+
+    //    private FormulaTree<T> parent;
+    //    /// <summary>
+    //    /// 父结点
+    //    /// </summary>
+    //    public FormulaTree<T> Parent
+    //    {
+    //        get { return parent; }
+    //    }
+    //    /// <summary>
+    //    /// 结点数据
+    //    /// </summary>
+    //    public T Data { get; set; }
+
+    //    private List<FormulaTree<T>> nodes;
+    //    /// <summary>
+    //    /// 子结点
+    //    /// </summary>
+    //    public List<FormulaTree<T>> Nodes
+    //    {
+    //        get { return nodes; }
+    //    }
+    //    /// <summary>
+    //    /// 添加结点
+    //    /// </summary>
+    //    /// <param name="node">结点</param>
+    //    public void AddNode(FormulaTree<T> node)
+    //    {
+    //        if (!nodes.Contains(node))
+    //        {
+    //            node.parent = this;
+    //            nodes.Add(node);
+    //        }
+    //    }
+    //    /// <summary>
+    //    /// 添加结点
+    //    /// </summary>
+    //    /// <param name="nodes">结点集合</param>
+    //    public void AddNode(List<FormulaTree<T>> nodes)
+    //    {
+    //        foreach (var node in nodes)
+    //        {
+    //            if (!nodes.Contains(node))
+    //            {
+    //                node.parent = this;
+    //                nodes.Add(node);
+    //            }
+    //        }
+    //    }
+    //    /// <summary>
+    //    /// 移除结点
+    //    /// </summary>
+    //    /// <param name="node"></param>
+    //    public void Remove(FormulaTree<T> node)
+    //    {
+    //        if (nodes.Contains(node))
+    //            nodes.Remove(node);
+    //    }
+    //    /// <summary>
+    //    /// 清空结点集合
+    //    /// </summary>
+    //    public void RemoveAll()
+    //    {
+    //        nodes.Clear();
+    //    }
+    //}
+
+    //public static class Extensions
+    //{
+    //    public static string GetString<K, V>(this IDictionary<K, V> dict)
+    //    {
+    //        var items = dict.Select(kvp => kvp.ToString());
+    //        return "{" + string.Join(", ", items) + "}";
+    //    }
+    //}
+
+}
